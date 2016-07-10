d6dbe40165f0c4bc56222ebb234e8a0499dfbe01
dillon <dillon@YZLI-WEB-DEVELO>
2016-07-10 12:43:57 +0800

    Add project files.

---
+++ TDD-Day2-Homework/PotterShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Day2_Homework
+{
+    public class PotterShoppingCart
+    {
+        public int checkout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDD-Day2-HomeworkTests/PotterShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Day2_Homework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Day2_Homework.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void checkoutTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
