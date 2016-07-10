1443e68cbd00d971ba13971a32c7671e51947db0
dillon <dillon@YZLI-WEB-DEVELO>
2016-07-10 14:05:11 +0800

    撰寫對應測試案例 2 的測試程式，執行為紅燈

--- TDD-Day2-HomeworkTests/PotterShoppingCartTests.cs
+++ TDD-Day2-HomeworkTests/PotterShoppingCartTests.cs
@@ -29,5 +29,26 @@ namespace TDD_Day2_Homework.Tests
             // assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void Test_Checkout_Buy_1_Volume1_And_1_Volume2_Checkout_should_be_190()
+        {
+            // arrange
+            var target = new PotterShoppingCart();
+            var potterBooks = new List<PotterBook>
+            {
+                new PotterBook() { Volume = 1 },
+                new PotterBook() { Volume = 2 }
+            };
+            target.AddPotterBooks(potterBooks);
+
+            var expected = 190;
+
+            // act
+            var actual = target.checkout();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
