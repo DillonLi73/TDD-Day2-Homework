82720ffe9caf93ac40b6655eea8f4e03074d1ac2
dillon <dillon@YZLI-WEB-DEVELO>
2016-07-10 16:45:14 +0800

    修改 production code，讓測試綠燈

--- TDD-Day2-Homework/PotterShoppingCart.cs
+++ TDD-Day2-Homework/PotterShoppingCart.cs
@@ -11,7 +11,55 @@ namespace TDD_Day2_Homework
 
         public int checkout()
         {
-            return 100 * this._potterBooks.Count();
+            if ( this._potterBooks == null || this._potterBooks.Count() == 0 ) { return 0; }
+
+            int amount = 0;
+
+            var countByVolume = new Dictionary<int, int>();
+            foreach (var potterBook in this._potterBooks)
+            {
+                if (countByVolume.ContainsKey(potterBook.Volume))
+                {
+                    countByVolume[potterBook.Volume] += 1;
+                }
+                else
+                {
+                    countByVolume[potterBook.Volume] = 1;
+                }
+            }
+
+            // process 2 different volume case: 5% discount
+            List<int> volumes = null;
+            while (countByVolume.Count >= 2)
+            {
+                amount += (int)(100 * 2 * 0.95);
+
+                int bookMinusNum = 2;
+                volumes = countByVolume.Keys.ToList();
+                foreach (var volume in volumes)
+                {
+                    if (countByVolume[volume] > 0)
+                    {
+                        countByVolume[volume] -= 1;
+                        if (countByVolume[volume] == 0)
+                        {
+                            countByVolume.Remove(volume);
+                        }
+                        bookMinusNum -= 1;
+                    }
+
+                    if (bookMinusNum == 0) { break; }
+                }   
+            }
+
+            // process normal case: no discount
+            volumes = countByVolume.Keys.ToList();
+            foreach (var volume in volumes)
+            {
+                amount += 100 * countByVolume[volume];
+            }
+
+            return amount;
         }
 
         internal void AddPotterBooks(List<PotterBook> potterBooks)
